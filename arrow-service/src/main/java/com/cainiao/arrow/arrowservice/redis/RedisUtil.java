a96253f74db0d391e057f1dae5325a6fe840d696
nhq <429704414@qq.com>
2021-01-01 20:46:33 +0800

    新加了处理表格的类

--- arrow-service/src/main/java/com/cainiao/arrow/arrowservice/redis/RedisUtil.java
+++ arrow-service/src/main/java/com/cainiao/arrow/arrowservice/redis/RedisUtil.java
@@ -3,6 +3,7 @@ import redis.clients.jedis.Jedis;
 
 import java.util.HashMap;
 import java.util.Hashtable;
+import java.util.Random;
 
 public class RedisUtil {
 
@@ -11,12 +12,40 @@ public class RedisUtil {
     private static int port=6379;
 
     public static void main(String[] args) {
-        Jedis jedis=new Jedis(addr,port);
-        String val = jedis.get("yourKey");
-        System.out.println(val);
-        HashMap hashMap = new HashMap<>();
-       
+//        Jedis jedis=new Jedis(addr,port);
+//        String val = jedis.get("yourKey");
+//        System.out.println(val);
+//        HashMap hashMap = new HashMap<>();
+//
+        testLindex();
     }
 
+    public static void testLindex(){
+        Jedis jedis=new Jedis(addr,port);
+        Random random = new Random();
+
+        jedis.del("mylist");
+        System.out.println(jedis.llen("mylist"));
+        int len = 100000;
+        String key = "mylist";
+        for(int i=0;i<len;i++){
+            jedis.lpush(key,i+"");
+        }
+        long curr = System.currentTimeMillis();
+//        for(int i=0;i<len;i++){
+//            String res = jedis.lindex(key,i);
+////            if(i<10){
+////                System.out.println(res);
+////            }
+//        }
+//        System.out.println(System.currentTimeMillis()-curr);
+
+        long curr2 = System.currentTimeMillis();
+        for(int i=0;i<len;i++){
+            jedis.lpop(key);
+        }
+        System.out.println(System.currentTimeMillis()-curr2);
+        System.out.println(jedis.llen(key));
+    }
 
 }
--- arrow-service/src/main/java/com/cainiao/arrow/arrowservice/xlsx/XlsxUtil.java
+++ arrow-service/src/main/java/com/cainiao/arrow/arrowservice/xlsx/XlsxUtil.java
@@ -1,5 +1,7 @@
 package com.cainiao.arrow.arrowservice.xlsx;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -17,18 +19,26 @@ public class XlsxUtil {
     public static void main(String[] args) {
 
         try {
-            String fileName = "/Users/huqingnie/Downloads/WPS文档/映射文件.xlsx";
+            String fileName = "/Users/huqingnie/Downloads/入账表格/流水/11月杭州云客赞0989明细.xlsx";
             XSSFWorkbook xwb = new XSSFWorkbook(new FileInputStream(fileName));
             Map<String, List<String>> shopIdMap = new HashMap<String, List<String>>();
-            String fillStr = "";    //存储aaa文件里的数据
-            String[] fillSplit = null;
-            XSSFSheet xSheet = xwb.getSheetAt(2);
+            /**
+             *  1、选取哪个sheet（从0开始数）
+             */
+            XSSFSheet xSheet = xwb.getSheetAt(5);
             for (int i = 0; i <= xSheet.getLastRowNum(); i++) {
                 if (xSheet.getRow(i) == null) {
                     continue;
                 }
-                String currKey = xSheet.getRow(i).getCell(3).toString();
-                String currVal = xSheet.getRow(i).getCell(2).toString();
+
+                /**
+                 *  2、数据源的名称，所在的列号（从0开始数）
+                 */
+                String currKey = xSheet.getRow(i).getCell(6).toString();
+                /**
+                 *  3、数据源对应的ID，所在的列号（从0开始数）
+                 */
+                String currVal = xSheet.getRow(i).getCell(5).toString();
                 if(!shopIdMap.containsKey(currKey)){
                     List<String> list= new ArrayList<>();
                     list.add(currVal);
@@ -39,26 +49,23 @@ public class XlsxUtil {
                 if(shopIdMap.get(currKey).size()>1){
                     System.out.println(currKey);
                 }
-//                for (int j = 0; j <= xSheet.getRow(i).getPhysicalNumberOfCells(); j++) {  //遍历当前行的所有列
-//                    if (xSheet.getRow(i).getCell(j) == null) {//为空执行下次循环
-//                         System.out.println("qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq");
-//                        continue;
-//                    }
-//                    fillStr = (xSheet.getRow(i)).getCell(j).toString();//获取当前单元格的数据
-//                    fillSplit = fillStr.split("_");//切割，本人的数据是以"_"为分隔符的这个可以根据自己情况改变
-////                        XSSFRow xRow = xSheet.createRow(i);
-////                        XSSFCell xCell = xRow.createCell(j);
-//                    XSSFCell xCell = xSheet.getRow(i).getCell(j); //获取单元格对象，这块不能向上边那两句代码那么写，不能用createXXX，用的话会只把第一列的数据改掉
-//                    //xCell.setCellValue(fields.get(fillSplit[0].trim()) == null ? fillStr : fields.get(fillSplit[0].trim()));//修改数据，看数据是否和字段集合中的数据匹配，不匹配使用元数据
-//                     System.out.println(JSON.toJSONString(xCell));
-//                }
             }
             for (int i = 0; i <= xSheet.getLastRowNum(); i++) {  //遍历所有的行
                 if (xSheet.getRow(i) == null || xSheet.getRow(i).getCell(0) == null) { //这行为空执行下次循环
                     continue;
                 }
-                String currKey = xSheet.getRow(i).getCell(0).toString();
-                XSSFCell xCell = xSheet.getRow(i).createCell(1);
+                /**
+                 * 4、需要匹配的名称，所在的列号（从0开始数）
+                 */
+                int matchCellNum = 2;
+                if(xSheet.getRow(i).getCell(matchCellNum)==null||StringUtils.isEmpty(xSheet.getRow(i).getCell(matchCellNum).toString())){
+                    continue;
+                }
+                String currKey = xSheet.getRow(i).getCell(matchCellNum).toString();
+                /**
+                 *  5、在哪一列生成这个匹配得到的数据（从0开始数）
+                 */
+                XSSFCell xCell = xSheet.getRow(i).createCell(3);
                 if(shopIdMap.get(currKey)==null){
                     continue;
                 }
@@ -71,7 +78,7 @@ public class XlsxUtil {
             xwb.write(out);
             out.close();
         } catch (Exception e) {
-            System.out.println(e);
+            throw new RuntimeException(e);
         }
 
     }
